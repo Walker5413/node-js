c6e7c1696a3592ee9e2e70ee2fd797f0670017f4
Walker5413 <Kaleabbirhanu.kbw@gmail.com>
2022-08-25 11:50:35 +0300

    routing

--- pipe_2server.js
+++ pipe_2server.js
@@ -3,8 +3,11 @@ const fs = require('fs');
 // ***piping text file to server
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'content-Type': 'text/plain'});
-    const readStream = fs.createReadStream(__dirname + '/lorem.txt', 'utf-8');
+    // res.writeHead(200, {'content-Type': 'text/plain'});
+    // const readStream = fs.createReadStream(__dirname + '/lorem.txt', 'utf-8');
+    res.writeHead(200, {'content-Type': 'text/html'});
+    const readStream = fs.createReadStream(__dirname + '/index.html', 'utf-8');
+    //
     readStream.pipe(res)
 });
 
---
+++ routing/app.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+// ***serving json data to node
+
+const server = http.createServer((req, res) => {
+    // ***routing
+   
+    // res.writeHead(200, {'content-Type': 'application/json'});
+   
+    // console.log(req.url);
+   
+    // const person = {
+    //     name: 'alex',
+    //     email: 'alex@gmail.com',
+    //     job: 'designer'
+    // };
+    
+    // res.end(JSON.stringify(person));
+    //
+    if(req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }
+    else if(req.url === '/about'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/about.html').pipe(res);
+    }
+    else{
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
+});
+
+server.listen(3000, '127.0.0.1');
