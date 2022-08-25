8d53a6f657372274c8c92b6463aeecb090abc00c
Walker5413 <Kaleabbirhanu.kbw@gmail.com>
2022-08-25 18:50:33 +0300

    ejs

--- express/app.js
+++ express/app.js
@@ -2,18 +2,22 @@ const express = require('express');
 
 const app = express();
 
+
 // ***to set the view engine for express
 app.set('view engine', 'ejs');
 //
-app.get('/home', (req, res) =>{
-    res.sendFile(__dirname + '/index.html');
+app.use('/assets', express.static('assets'));
+
+app.get('/', (req, res) =>{
+    res.render('index');
 });
 app.get('/about', (req, res) =>{
-    res.sendFile(__dirname + '/about.html');
+    console.log(req.query);
+    res.render('about', {qs: req.query});
 });
 // ***injuction of dynamic data
 app.get('/profile/:name', (req, res) =>{
-    const data = {age: 30, job: 'developer'}
+    const data = {age: 30, job: 'developer', friends: [ 'sarah', 'jane', 'william']}
     res.render('profile', {person: req.params.name, data});
 });
 app.listen(3000); 
