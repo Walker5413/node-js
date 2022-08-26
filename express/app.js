0f550c4924f0ca2af33842aaf46d0252061656cb
Walker5413 <Kaleabbirhanu.kbw@gmail.com>
2022-08-26 13:58:26 +0300

    end

--- express/app.js
+++ express/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-
+const bodyparser =require('body-parser');
 const app = express();
 
+// ***body parser
+const urlencodedParser = bodyparser.urlencoded({ extended: false});
 
 // ***to set the view engine for express
 app.set('view engine', 'ejs');
@@ -9,12 +11,21 @@ app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) =>{
+    console.log(req.query);
     res.render('index');
 });
+
 app.get('/about', (req, res) =>{
-    console.log(req.query);
+    console.log(req.body);
     res.render('about', {qs: req.query});
 });
+
+
+app.post('/about', urlencodedParser, (req, res) =>{
+    res.render('about-success', {data: req.body});
+});
+
+
 // ***injuction of dynamic data
 app.get('/profile/:name', (req, res) =>{
     const data = {age: 30, job: 'developer', friends: [ 'sarah', 'jane', 'william']}
